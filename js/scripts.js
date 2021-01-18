58352e338979f7d0ca2c8448ce6d1492baafcee8
Conner Farrant <conner_f@outlook.com>
2021-01-18 22:50:58 +1300

    new site

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var notepad = document.getElementById("main_education");
+notepad.onclick = function(e) {
+  window.location.href = "#notepad";
+}
+
+var phone = document.getElementById("main_apps");
+phone.onclick = function(e) {
+  window.location.href = "#phone";
+}
+
+var laptop = document.getElementById("main_projects");
+laptop.onclick = function(e) {
+  window.location.href = "https://github.com/ciegeNZ";
+}
